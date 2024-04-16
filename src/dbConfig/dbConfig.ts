eded3f824b3c3629c07a6247fdfd37101e15a707
Ikujebi <133238121+Ikujebi@users.noreply.github.com>
2024-04-16 17:30:27 +0100

    ds

--- src/dbConfig/dbConfig.ts
+++ src/dbConfig/dbConfig.ts
@@ -6,7 +6,14 @@ mongoose.connect(process.env.MONGO_URI!);
 const connection = mongoose.connection
 
 connection.on("connected", () =>{
-    console.log("connected successfully");
+    console.log("MongoDB connected successfully");
+
+    connection.on('error', (err) => {
+        console.log('MongoDB connection error. Please make sure MongoDB is running.' + err);
+
+        process.exit();
+        
+    })
     
 })
     }catch(error){
