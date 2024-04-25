f4a92afd36eecfd0761e5fb09adca0f91e103ecf
Ikujebi <133238121+Ikujebi@users.noreply.github.com>
2024-04-25 09:36:57 +0100

    login

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -2,11 +2,20 @@
 import Link from "next/link"
 import React,{useState} from "react"
 import { useRouter } from "next/navigation"
-import { Axios } from "axios"
+import axios from "axios"
+import { FaEye } from "react-icons/fa";
+import { FaEyeSlash  } from "react-icons/fa";
 
 
 
 export default function LoginPage() {
+    const OpenEye = <FaEye />
+    const CloseEye = <FaEyeSlash  />
+    const [showPassword, setShowPassword] = useState(false); // Step 1
+
+    const togglePasswordVisibility = () => {
+        setShowPassword((prev) => !prev); 
+    };
     const [user, setUser] = useState({
 email: "",
 password: "",
@@ -28,18 +37,26 @@ password: "",
            
             <label htmlFor="email">email</label>
             <input
-            className="p-2 border border-gray-300 rounded-xl mb-2 focus:outline-none  focus:border-gray-600"
+            className="p-2 text-gray-700 border border-gray-300 rounded-xl mb-2 focus:outline-none  focus:border-gray-600"
             id="email" type="email" value={user.email} placeholder="email" onChange={(e) => setUser
              ({...user, email:e.target.value})}/>
             <label htmlFor="password">password</label>
+
+            <div className="relative">
             <input
-            className="p-2 border border-gray-300 rounded-xl mb-2 focus:outline-none  focus:border-gray-600"
-            id="password" type="password" value={user.password} placeholder="password" onChange={(e) => setUser
+            className="p-2 text-gray-700 border border-gray-300 rounded-xl mb-2 focus:outline-none  focus:border-gray-600"
+            id="password" type={showPassword ? "text": "password"} value={user.password} placeholder="password" onChange={(e) => setUser
              ({...user, password:e.target.value})}/>
-             
+             <button
+             type="button"
+             className="absolute top-1/2 right-2 transform -translate-y-1/2 text-gray-600 "
+             onClick={togglePasswordVisibility}>
+                    {showPassword ? OpenEye : CloseEye}
+             </button>
+             </div>
              <button 
              onClick={onLogin}
-             className="p-2 border border-gray-300 rounded-2xl mb-4 focus:outline-none  focus:border-gray-600">Login here</button>
+             className="p-2 border border-gray-300 rounded-2xl mb-4 focus:outline-none  focus:border-gray-600 text">Login here</button>
              </div></div>
             <p>
                 <Link href={"/signup"}>Visit Signup Page</Link>
