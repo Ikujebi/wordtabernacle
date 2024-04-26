099debe6d0dd25457b6b4fcfeec7223e48b3378a
Ikujebi <133238121+Ikujebi@users.noreply.github.com>
2024-04-26 14:46:46 +0100

    logout

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,11 +1,36 @@
-
+"use client"
+import axios from "axios"
+import Link from "next/link"
+import toast from "react-hot-toast"
+import { useRouter } from "next/navigation"
 
 const profilePage = () => {
+
+  const router = useRouter()
+
+const logout = async () =>{
+  try {
+  await  axios.get('api/users/logout')
+  router.push("/login")
+    toast.success("Successfully logged out");
+  } catch (error:any) {
+    console.log(error.message);
+    toast.error(error.message)
+    
+  }
+}
+
   return (
         <div className="flex bg-black text-white flex-col items-center justify-center min-h-screen py-2">
             <h1>Profile</h1>
             <hr />
         <p>Profile page</p>
+
+        <hr />
+
+        <button
+        onClick={logout}
+        className="bg-blue-500 hover:bg-blue-700 text-white py-2 px-4 mt-5 font-bold rounded">Logout</button>
             
             </div>
   )
