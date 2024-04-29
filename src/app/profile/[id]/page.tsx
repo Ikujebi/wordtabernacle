bb36707207c366966f692611983e2ba836c0df95
Ikujebi <133238121+Ikujebi@users.noreply.github.com>
2024-04-29 13:56:13 +0100

    tester mailing

--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -3,7 +3,7 @@
 const useProfile = ({params}: any) => {
     return (
           <div className="flex bg-black text-white flex-col items-center justify-center min-h-screen py-2">
-              <h1>Profile</h1>
+              <h1 className="mb-10">Profile</h1>
               <hr />
           <p className="text-3xl">Profile page <span className="p-2 ml-2 rounded-xl bg-orange-500 text-black">{params.id}</span></p>
               
