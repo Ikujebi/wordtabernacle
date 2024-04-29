10636e7cf781848c2f36152ca041d9411df56e9b
Ikujebi <133238121+Ikujebi@users.noreply.github.com>
2024-04-29 09:25:36 +0100

    Update page.tsx

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -37,13 +37,17 @@ const profilePage = () => {
       <h1>Profile</h1>
       <hr />
       <p>Profile page</p>
-<h2>{data === "nothing!!!" ? "Nothing!!!" : <Link href={`/profile/${data}`}></Link>}</h2>
+<h2 className="p-2 rounded bg-orange-500 mt-2">{data === "nothing!!!" ? "Nothing!!!" : <Link href={`/profile/${data}`}>{data}</Link>}</h2>
       <hr />
+
+      <button
+        onClick={getUserDetails}
+        className="bg-green-500 hover:bg-blue-700 text-white py-2 px-4 mt-5 font-bold rounded">Get User Details</button>
 
       <button
         onClick={logout}
         className="bg-blue-500 hover:bg-blue-700 text-white py-2 px-4 mt-5 font-bold rounded">Logout</button>
-      <Link className="bg-green-500 hover:bg-green-700 text-white py-2 px-4 mt-15 font-bold rounded" href={"/"}>
+      <Link className="bg-pink-500 hover:bg-green-700 text-white py-2 px-4 mt-[20rem] ml-[20rem] font-bold rounded" href={"/"}>
         HOME
       </Link >
     </div>
