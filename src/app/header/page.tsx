2a3dd92687e618cb4688f26ad8ee6e899e83e71d
Ikujebi <133238121+Ikujebi@users.noreply.github.com>
2024-05-17 17:31:28 +0100

    started working on nav

--- src/app/header/page.tsx
+++ src/app/header/page.tsx
@@ -1,10 +1,11 @@
+"use client"
 import {     Button } from "antd"
 import Image from "next/image";
 import { MdClose, MdEmail, MdMenu, MdOndemandVideo } from 'react-icons/md';
 import 'animate.css';
 
 import Link from "next/link";
-import wordlogo from '../../img/wordlogo.png'
+import wordlogo from '../img/wordlogo.png'
 import { useState } from "react";
 import { useRouter } from "next/navigation";
 import { FaFacebookF, FaInstagram, FaTwitter, FaYoutube } from "react-icons/fa";
@@ -15,56 +16,57 @@ const Header = () => {
     return (
         <div className="w-[100%] bg-white  fixed z-20 pb-10 animate__animated animate__fadeInDown">
     <div className="w-full hidden md:block">
-        <div className="h-10 gap-5 bg-white border-b border-gray-200 flex px-14 justify-between">
-            <div>
-                <span className="text-red-600 font-semibold text-sm">
+        <div className="h-10 gap-5 bg-white border-b border-gray-200 flex px-14 justify-between ">
+            <div className="mt-3">
+                <span className="text-red-600  font-semibold text-sm md:text-[1.2rem]">
                     <Link href={"/login"}>Login</Link>
                 </span>
             </div>
             <div className="flex items-center text-gray-600 text-sm">
-                <div className="w-8 h-3 bg-red-600 rounded-full flex items-center justify-center">
+                <div className="w-8 h-8 bg-red-600 rounded-full flex items-center justify-center">
                     <FaTwitter className="text-white text-lg" />
                 </div>
-                <div className="w-8 h-3 bg-red-600 rounded-full flex items-center justify-center">
+                <div className="w-8 h-8 bg-red-600 rounded-full flex items-center justify-center">
                     <FaInstagram className="text-white text-lg" />
                 </div>
-                <div className="w-8 h-3 bg-red-600 rounded-full flex items-center justify-center">
+                <div className="w-8 h-8 bg-red-600 rounded-full flex items-center justify-center">
                     <FaFacebookF className="text-white text-lg" />
                 </div>
-                <div className="w-8 h-3 bg-red-600 rounded-full flex items-center justify-center">
+                <div className="w-8 h-8 bg-red-600 rounded-full flex items-center justify-center">
                     <FaYoutube className="text-white text-lg" />
                 </div>
-            </div>
-            <div className="ml-2 flex items-center">
+                <div className="ml-2 flex items-center">
                 <MdEmail />
                 <span className="ml-1">wordtabernacle@gmail.com</span>
             </div>
+            </div>
+           
         </div>
         <div className="px-12">
             <div className="h-10 bg-white flex mt-20 justify-between">
                 <div>
                     <Link href={"#"}>
                         <div className="flex items-center">
-                            <div className="ml-40">
-                                <Image alt="church logo" width={300} height={100} src={wordlogo} className="h-10" />
+                            <div className="">
+                                <Image alt="church logo" width={300} height={100} src={wordlogo} className="h-10 w-10" />
                             </div>
                             <span className="font-semibold text-sm text-red-600">WORD TABERNACLE</span>
                         </div>
                     </Link>
                 </div>
-                <div className="flex space-x-40 md:space-x-70 text-gray-600 text-sm">
+                <div className="flex gap-10  text-gray-600 text-sm">
                     <Link href={"#"}>
-                        <div className="flex items-center">
+                         <div className="flex items-center">
                             <MdOndemandVideo />
                             <span className="ml-1">Online</span>
-                        </div>
+                        </div> 
                     </Link>
                     <Link href={"#"}>About</Link>
                     <Link href={"#"}>Contact</Link>
                     <Link href={"#"}>Community</Link>
                     <Link href={"#"}>Announcement</Link>
                 </div>
-                <Button className="text-white bg-red-600 hover:bg-red-700 py-2 px-4 rounded-sm" onClick={() => {routes.push("/login")}}>LOGIN</Button>
+                <Button className="text-white bg-red-600 hover:bg-red-700 m-auto w-[10%] h-[110%] rounded-lg" onClick={() => {routes.push("/login")}}>LOGIN</Button>
             </div>
         </div>
     </div>
