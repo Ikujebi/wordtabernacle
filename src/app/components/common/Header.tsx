d65bd72e38d9d03ab47962a86e4af527af42b6d0
Ikujebi <133238121+Ikujebi@users.noreply.github.com>
2024-05-28 10:35:12 +0100

    mobile

--- src/app/components/common/Header.tsx
+++ src/app/components/common/Header.tsx
@@ -3,7 +3,6 @@ import {     Button } from "antd"
 import Image from "next/image";
 import { MdClose, MdEmail, MdMenu, MdOndemandVideo } from 'react-icons/md';
 import 'animate.css';
-
 import Link from "next/link";
 import wordlogo from '../../img/wordlogo.png'
 import React, { useState } from 'react';
@@ -15,24 +14,24 @@ const Header = () => {
     let [display, changeDisplay] = useState('none')
     return (
         <div className="w-[100%] bg-white fixed z-20 pb-3 mb-13 animate__animated animate__fadeInDown">
-            <div className="w-full hidden md:block lg:block xl:block 2xl:block">
-                <div className="h-10 gap-5 bg-white border-b border-gray-200 flex px-14 justify-between">
-                    <div className="mt-3">
-                        <span className="text-red-600 font-semibold text-sm md:text-[1.2rem]">
+            <div className="w-full ">
+                <div className="h-10 gap-5 bg-white border-b border-gray-200 flex lg:px-14 xl:px-14 2xl:px-14  md:px-14 justify-between">
+                    <div className="mt-2">
+                        <span className="text-red-600 font-semibold text-[1.2rem] ">
                             <Link href={"/login"}>Login</Link>
                         </span>
                     </div>
                     <div className="flex items-center text-gray-600 text-sm">
-                        <div className="w-8 h-8 bg-red-600 rounded-full flex items-center justify-center">
+                        <div className="md:w-8 lg:w-8 xl:w-8 2xl:w-8 w-6 md:h-8 lg:h-8 xl:h-8 2xl:h-8 h-6 bg-red-600 rounded-full flex items-center justify-center">
                             <FaTwitter className="text-white text-lg" />
                         </div>
-                        <div className="w-8 h-8 bg-red-600 rounded-full flex items-center justify-center">
+                        <div className="md:w-8 lg:w-8 xl:w-8 2xl:w-8 w-6 md:h-8 lg:h-8 xl:h-8 2xl:h-8 h-6 bg-red-600 rounded-full flex items-center justify-center">
                             <FaInstagram className="text-white text-lg" />
                         </div>
-                        <div className="w-8 h-8 bg-red-600 rounded-full flex items-center justify-center">
+                        <div className="md:w-8 lg:w-8 xl:w-8 2xl:w-8 w-6 md:h-8 lg:h-8 xl:h-8 2xl:h-8 h-6 bg-red-600 rounded-full flex items-center justify-center">
                             <FaFacebookF className="text-white text-lg" />
                         </div>
-                        <div className="w-8 h-8 bg-red-600 rounded-full flex items-center justify-center">
+                        <div className="md:w-8 lg:w-8 xl:w-8 2xl:w-8 w-6 md:h-8 lg:h-8 xl:h-8 2xl:h-8 h-6 bg-red-600 rounded-full flex items-center justify-center">
                             <FaYoutube className="text-white text-lg" />
                         </div>
                         <div className="ml-2 flex items-center">
@@ -41,15 +40,15 @@ const Header = () => {
                         </div>
                     </div>
                 </div>
-                <div className="">
+                <div className="hidden md:block lg:block xl:block 2xl:block">
                     <div id="nav" className="h-10 w-[100svw] flex mt-10 justify-between px-14">
                         <div>
                             <Link href={"#"}>
-                                <div className="flex">
+                                <div className="flex gap-1">
                                     <div className="">
                                         <Image alt="church logo" width={300} height={100} src={wordlogo} className="h-10 w-10" />
                                     </div>
-                                    <span className="font-semibold text-sm text-red-600">WORD TABERNACLE</span>
+                                    <span className="font-semibold  text-red-600 mt-3">WORD TABERNACLE</span>
                                 </div>
                             </Link>
                         </div>
@@ -69,21 +68,22 @@ const Header = () => {
                     </div>
                 </div>
             </div>
-            <div className="items-center mt-5 md:hidden lg:hidden xl:hidden 2xl:hidden">
+            <div className="items-center flex justify-between mt-5 md:hidden lg:hidden xl:hidden 2xl:hidden">
                 <Link href={"#"}>
                     <div className="flex items-center">
                         <div className="ml-4">
-                            <Image alt="church logo" width={300} height={100} src={wordlogo} className="h-5 w-5" />
+                            <Image alt="church logo" width={300} height={100} src={wordlogo} className="h-6 w-6" />
                         </div>
-                        <span className="font-semibold text-sm text-red-600">WORD TABERNACLE</span>
+                        <span className="font-semibold text-[.67rem] text-red-600">WORD <br /> TABERNACLE</span>
                     </div>
                 </Link>
-            </div>
-            <div className="mt-5 hidden">
-                <Button className="bg-#FFFAFA mr-2" aria-label="Open Menu" onClick={() => { changeDisplay('flex') }}>
+                <div className=" ">
+                <Button id="openmenu" className="bg-#FFFAFA mr-2" aria-label="Open Menu" onClick={() => { changeDisplay('flex') }}>
                     <MdMenu className="text-gray-700" />
                 </Button>
             </div>
+            </div>
+           
             <div className="fixed top-0 left-0 z-20 w-screen h-screen bg-#FFFAFA flex flex-col overflow-auto" style={{ display: display }}>
                 <div className="mt-5 justify-end flex">
                     <Button className="bg-#FFFAFA mr-6" aria-label="Close Menu" onClick={() => { changeDisplay('none') }}>
--- src/app/header/page.tsx
+++ src/app/header/page.tsx
@@ -1,39 +1,113 @@
-import choir from '../img/choir.webp';
-import cardimg from '../img/youthpreacher.webp';
-import cardimg2 from '../img/testimony.webp';
+"use client"
+import {     Button } from "antd"
+import Image from "next/image";
+import { MdClose, MdEmail, MdMenu, MdOndemandVideo } from 'react-icons/md';
+import 'animate.css';
 
-const Community = () => {
-  return (
-    <div className="flex w-full h-screen items-center">
-      <section className="w-[33.3%]">
-        <div className="content h-[30rem] relative" style={{ backgroundImage: `url(${choir.src})`, backgroundSize: 'cover', backgroundPosition: 'center' }}>
-          <div className="p-4 absolute bottom-0 left-0 right-0 text-center pb-[5rem]">
-            <h2 className="text-white text-center font-semibold">We're Joyful</h2>
-            <p className="text-white text-center font-semibold text-sm md:text-base">BECOME<br />A<br />MEMBER</p>
-            <button className="bg-red-600 text-white w-[50%] p-3 mt-5">JOIN US</button>
-          </div>
-        </div>
-      </section>
-      <section className="w-[33.3%]">
-        <div className="content h-[30rem] relative" style={{ backgroundImage: `url(${cardimg.src})`, backgroundSize: 'cover', backgroundPosition: 'center' }}>
-          <div className="p-4 absolute bottom-0 left-0 right-0 text-center pb-[5rem]">
-            <h2 className="text-white text-center font-semibold">Build Capacity</h2>
-            <p className="text-white text-center font-semibold text-lg md:text-xl">JOIN OUR<br />LEADERSHIP<br />CLASS</p>
-            <button className="bg-red-600 text-white w-[50%] p-3 mt-5">START HERE</button>
-          </div>
-        </div>
-      </section>
-      <section className="w-[33.3%]">
-        <div className="content h-[30rem] relative" style={{ backgroundImage: `url(${cardimg2.src})`, backgroundSize: 'cover', backgroundPosition: 'center' }}>
-          <div className="p-4 absolute bottom-0 left-0 right-0 text-center pb-[5rem]">
-            <h2 className="text-white text-center font-semibold">Prophetic session</h2>
-            <p className="text-white text-center font-semibold text-lg md:text-xl">REQUEST<br />A<br />PRAYER SESSION</p>
-            <button className="bg-red-600 text-white w-[50%] p-3 mt-5 ">LETS PRAY</button>
-          </div>
-        </div>
-      </section>
-    </div>
-  );
-};
+import Link from "next/link";
+import wordlogo from '../img/wordlogo.png'
+import React, { useState } from 'react';
+import { useRouter } from "next/navigation";
+import { FaFacebookF, FaInstagram, FaTwitter, FaYoutube } from "react-icons/fa";
 
-export default Community;
+const Header = () => {
+    let routes = useRouter();
+    let [display, changeDisplay] = useState('none')
+    return (
+        <div className="w-[100%] bg-white fixed z-20 pb-3 mb-13 animate__animated animate__fadeInDown">
+            <div className="w-full ">
+                <div className="h-10 gap-5 bg-white border-b border-gray-200 flex lg:px-14 xl:px-14 2xl:px-14  md:px-14 justify-between">
+                    <div className="mt-2">
+                        <span className="text-red-600 font-semibold text-[1.2rem] ">
+                            <Link href={"/login"}>Login</Link>
+                        </span>
+                    </div>
+                    <div className="flex items-center text-gray-600 text-sm">
+                        <div className="md:w-8 lg:w-8 xl:w-8 2xl:w-8 w-6 md:h-8 lg:h-8 xl:h-8 2xl:h-8 h-6 bg-red-600 rounded-full flex items-center justify-center">
+                            <FaTwitter className="text-white text-lg" />
+                        </div>
+                        <div className="md:w-8 lg:w-8 xl:w-8 2xl:w-8 w-6 md:h-8 lg:h-8 xl:h-8 2xl:h-8 h-6 bg-red-600 rounded-full flex items-center justify-center">
+                            <FaInstagram className="text-white text-lg" />
+                        </div>
+                        <div className="md:w-8 lg:w-8 xl:w-8 2xl:w-8 w-6 md:h-8 lg:h-8 xl:h-8 2xl:h-8 h-6 bg-red-600 rounded-full flex items-center justify-center">
+                            <FaFacebookF className="text-white text-lg" />
+                        </div>
+                        <div className="md:w-8 lg:w-8 xl:w-8 2xl:w-8 w-6 md:h-8 lg:h-8 xl:h-8 2xl:h-8 h-6 bg-red-600 rounded-full flex items-center justify-center">
+                            <FaYoutube className="text-white text-lg" />
+                        </div>
+                        <div className="ml-2 flex items-center">
+                            <MdEmail />
+                            <span className="ml-1">wordtabernacle@gmail.com</span>
+                        </div>
+                    </div>
+                </div>
+                <div className="hidden md:block lg:block xl:block 2xl:block">
+                    <div id="nav" className="h-10 w-[100svw] flex mt-10 justify-between px-14">
+                        <div>
+                            <Link href={"#"}>
+                                <div className="flex gap-1">
+                                    <div className="">
+                                        <Image alt="church logo" width={300} height={100} src={wordlogo} className="h-10 w-10" />
+                                    </div>
+                                    <span className="font-semibold  text-red-600 mt-3">WORD TABERNACLE</span>
+                                </div>
+                            </Link>
+                        </div>
+                        <div className="flex items-center gap-5">
+                            <Link href={"#"}>
+                                <div className="flex">
+                                    <MdOndemandVideo />
+                                    <span className="ml-1">Online</span>
+                                </div>
+                            </Link>
+                            <Link href={"#"}>About</Link>
+                            <Link href={"#"}>Contact</Link>
+                            <Link href={"#"}>Community</Link>
+                            <Link href={"#"}>Announcement</Link>
+                            <Button className="text-white bg-red-600 hover:bg-red-700 m-auto rounded-lg" onClick={() => { routes.push("/login") }}>LOGIN</Button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="items-center flex justify-between mt-5 md:hidden lg:hidden xl:hidden 2xl:hidden">
+                <Link href={"#"}>
+                    <div className="flex items-center">
+                        <div className="ml-4">
+                            <Image alt="church logo" width={300} height={100} src={wordlogo} className="h-6 w-6" />
+                        </div>
+                        <span className="font-semibold text-[.67rem] text-red-600">WORD <br /> TABERNACLE</span>
+                    </div>
+                </Link>
+                <div className=" ">
+                <Button id="openmenu" className="bg-#FFFAFA mr-2" aria-label="Open Menu" onClick={() => { changeDisplay('flex') }}>
+                    <MdMenu className="text-gray-700" />
+                </Button>
+            </div>
+            </div>
+           
+            <div className="fixed top-0 left-0 z-20 w-screen h-screen bg-#FFFAFA flex flex-col overflow-auto" style={{ display: display }}>
+                <div className="mt-5 justify-end flex">
+                    <Button className="bg-#FFFAFA mr-6" aria-label="Close Menu" onClick={() => { changeDisplay('none') }}>
+                        <MdClose className="text-gray-700" />
+                    </Button>
+                </div>
+                <div className="flex flex-col items-center">
+                    <Link href={"#"}>LOGIN</Link>
+                    <Link href={"#"}>
+                        <div className="flex items-center">
+                            <MdOndemandVideo />
+                            <span className="ml-1">Online</span>
+                        </div>
+                    </Link>
+                    <Link href={"#"}>About</Link>
+                    <Link href={"#"}>Contact</Link>
+                    <Link href={"#"}>Community</Link>
+                    <Link href={"#"}>Announcement</Link>
+                </div>
+            </div>
+        </div>
+
+    )
+}
+
+export default Header;
