71a56544aad88c717ab8c51b20f5fb7013ab0f32
Ikujebi <133238121+Ikujebi@users.noreply.github.com>
2024-06-06 18:01:28 +0100

    Update page.tsx

--- src/app/header/page.tsx
+++ src/app/header/page.tsx
@@ -4,10 +4,8 @@ import Header from '../components/common/Header'
 import Footer from '../components/common/Footer'
 import Image from 'next/image'
 import givbg from '../img/giveimg.jpg'
+import pastor from '../img/pastor.webp'
 
-import redbg from '../img/redbgi.webp'
-import msgg from '../img/msg-1001955056097-3260.webp'
-import msgg2 from '../img/msg-1001955056097-3253.webp'
 
 
 const Page: FC = () => {
@@ -21,90 +19,57 @@ const Page: FC = () => {
         <Header />
       </header>
       <main className='pt-[10rem]'>
-        <div className="content  h-[6rem] relative flex justify-center items-center" style={{ backgroundImage: `url(${givbg.src})`, backgroundSize: 'cover', backgroundPosition: 'center' }}>
-          <h2 className='text-white font-semibold text-[1.9rem] relative inline-block'>
-            Vision, Mission, & Values
+      <div className="content  h-[6rem] 2xl:h-[8rem]  relative flex justify-center items-center" style={{ backgroundImage: `url(${givbg.src})`, backgroundSize: 'cover', backgroundPosition: 'center' }}>
+          <h2 className='text-white font-semibold 2xl:text-[1.9rem] text-[1.5rem] relative inline-block'>
+          Executive Leadership Team
             <span className='block absolute bottom-1 left-1/2 transform -translate-x-1/2 w-[100%] h-[4px] bg-white'></span>
           </h2>
         </div>
-        <div className='max-w-[67%]  text-[.83rem] xl:text-[.83rem] 2xl:text-[1.1rem] xl:w-[50%] 2xl:w-[40%]  mx-auto'>
-          <section className='mx-auto my-6 flex mb-[5rem]'>
-            <article>
-            <h2 className='text-[155%] font-semibold'>
-OUR VISION</h2>
-            <h3 className='text-[135%] font-semibold'>"Everyone Thriving"</h3>
-            <p className='text-[.96rem]'>
-              Word Tabernacle Church is more than a building consisting of four walls.
-               It is a spiritual vehicle for socio-economic,
-               system-level change committed to the Vision of “Everyone Thriving”.</p>
-
-            
-           
-            </article>
-
-            <article>
-              <Image width={1000} height={600} src={msgg2.src} alt="" className='w-[60rem] h-[13rem]' />
-            </article>
-          </section>
-          <section className='mx-auto my-6 flex mb-[5rem] gap-3'>
-  <article className='md:order-1 xl:order-1 lg:order-1 2xl:order-1 '>
-    <h2 className='text-[155%] font-semibold'>OUR MISSION</h2>
-    <h3 className='text-[135%] font-semibold'>"People empowering people and communities in every area of life to the glory of God."</h3>
-    <p  className=' xl:text-[1rem] 2xl:text-[1rem] text-[.96rem] mt-3'>Word Tabernacle Church is more than a building consisting of four walls. It is a spiritual vehicle for socio-economic, system-level change committed to the Vision of “Everyone Thriving”.</p>
-
-  </article>
-
-  <article className='md:order-0 lg:order-0 xl:order-0 2xl:order-0 '>
-    <Image className=' w-[60rem] h-[13rem]' width={1000} height={600} src={msgg.src} alt="" />
-  </article>
-</section>
-          
-          
-        
-        </div>
-        <div className="content  h-[6rem] relative flex justify-center items-center" style={{ backgroundImage: `url(${redbg.src})`, backgroundSize: 'cover', backgroundPosition: 'center' }}>
-          <h2 className='text-white font-semibold text-[1.9rem] relative inline-block'>
-          Our Values
-            <span className='block absolute bottom-1 left-1/2 transform -translate-x-1/2 w-[100%] h-[4px] bg-white'></span>
-          </h2>
-        </div>
-        <section className='flex flex-col justify-center items-center text-gray-600 mt-[3rem] '>
-          <article className='flex  w-[50rem]'>
-            <h2 className='font-semibold w-[13rem] text-[1.1rem] text-red-800'>
-            Accountability
-            </h2>
-            <div className='w-[30rem]'>
-            <p >
-We are personally responsible and accountable to one another
- for our attitude, actions, and integrity as a means of
-  accomplishing vision, building character,
- ensuring sound decision making, and growing as balanced disciples.
-            </p>
-            <p className='my-[2rem] text-[1rem]' >
-            (Acts 15:4; Job 31:6)
-            </p>
-            </div>
-            
+        <section className='flex  gap-[3rem] justify-center mt-[4rem]'>
+          <div className='flex w-[80%] gap-[3rem] '>
+          <article className=' w-[30%] '>
+            <Image width={1000} height={500} src={pastor.src} alt="pastor" className='w-full h-[35svh]' />
           </article>
-          <article className='flex w-[50rem]'>
-            <h2 className='font-semibold w-[13rem] text-[1.1rem] text-red-800'>
-            Authority
-            </h2>
-            <div className='w-[30rem]'>
-            <p>
-            The Holy Bible is divinely inspired by God and is the inerrant
-             authority by which God communicates, 
-            instructs, reveals, and directs our growth and our goals
+         
+          <article className='w-[60%]'>
+          <article>
+            <h1>
+            Ayodeji D. Ayorinde  |  Senior Pastor
+            </h1>
+            <p className='flex gap-5 text-[.8rem] underline'><a href="">Download Bio</a>
+            <a href="">Download Press Photos</a>
             </p>
-            <p className='my-[2rem] text-[1rem]'>
-            (2 Tim 3:16-17) 
-            </p>
-            </div>
-            
           </article>
+            <p className='mt-5'>
+            Pastor James David Gailliard is the Organizing and 
+            Senior Pastor of Word Tabernacle Church located in
+             Rocky Mount, North Carolina. At the intersection of
+              pastoral ministry and public policy he has formed 
+              a collaborative model of human, socio-political- 
+              economic and community development,
+               another avenue by which he has accomplished
+                this was by serving two consecutive terms 
+                in the North Carolina General Assembly, 
+                becoming “living history” as the only person 
+                of color to be elected to represent House District 25 
+                (Nash-Rocky Mount). As a Commissioner of MyFutureNC, 
+                he is one of the architects of the
+                 statewide educational attainment goal.
+            </p>
+            <p className='mt-8'>
+            Visionary, pioneer, leader, humanitarian,
+             and philanthropist, Pastor Gailliard was
+              named the 2016 Distinguished Citizen by the Rocky Mount Chamber of Commerce, and most recently recognized for making an impact within his community as the recipient of the 2023 President Joseph R. Biden, Jr. Lifetime Achievement Award. He is the Founder-Chairman of The REACH Center, Inc, a member of the Board of Trustees, Elizabeth City State University as well as the Board of Preachers, Morehouse College, his alma mater, and he is the former Chairman of the Board for the North Carolina Legislative Black Caucus Foundation. His academic background is an unusual combination
+             of theology, counseling, and business administration.
+            </p>
+            <p className='mt-8'>
+            Pastor Gailliard is a proud husband to Angela, with whom he shares a blended family of seven children and three grandchildren. Affectionately known as Pastor G, he is a spiritual father, son, and brother to many. To unwind, he can be found on his Harley Davidson or shooting a game of pool.
+            </p>
+          </article>
+          </div>
         </section>
       </main>
-      <footer>
+      <footer className=' w-full'>
         <Footer />
       </footer>
     </div>
