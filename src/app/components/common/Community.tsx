bf1ccde004242ea1e23072bb3a8bea4a4c43dbaa
Ikujebi <133238121+Ikujebi@users.noreply.github.com>
2024-06-10 09:08:30 +0100

    text

--- src/app/components/common/Community.tsx
+++ src/app/components/common/Community.tsx
@@ -8,8 +8,8 @@ const Community = () => {
       <section className="w-[33.3%]">
         <div className="content h-[30rem] relative" style={{ backgroundImage: `url(${choir.src})`, backgroundSize: 'cover', backgroundPosition: 'center' }}>
           <div className="p-4 absolute bottom-0 left-0 right-0 text-center pb-[5rem]">
-            <h2 className="text-white text-center font-semibold">We're Joyful</h2>
-            <p className="text-white text-center font-semibold text-sm md:text-base">BECOME<br />A<br />MEMBER</p>
+            <h2 className="text-white text-center font-semibold text-lg md:text-2xl xl:text-2xl 2xl:text-2xl">We're Joyful</h2>
+            <p className="text-white text-center font-semibold text-lg md:text-xl">BECOME<br />A<br />MEMBER</p>
             <button className="bg-red-600 text-white md:w-[50%] lg:w-[50%] xl:w-[50%] 2xl:w-[50%] p-3 mt-5 ">JOIN US</button>
           </div>
         </div>
@@ -17,7 +17,7 @@ const Community = () => {
       <section className="w-[33.3%]">
         <div className="content h-[30rem] relative" style={{ backgroundImage: `url(${cardimg.src})`, backgroundSize: 'cover', backgroundPosition: 'center' }}>
           <div className="p-4 absolute bottom-0 left-0 right-0 text-center pb-[5rem]">
-            <h2 className="text-white text-center font-semibold">Build Capacity</h2>
+            <h2 className="text-white text-center font-semibold text-lg md:text-2xl xl:text-2xl 2xl:text-2xl">Build Capacity</h2>
             <p className="text-white text-center font-semibold text-lg md:text-xl">JOIN OUR<br />LEADERSHIP<br />CLASS</p>
             <button className="bg-red-600 text-white  md:w-[50%] lg:w-[50%] xl:w-[50%] 2xl:w-[50%] p-3 mt-5">START HERE</button>
           </div>
@@ -26,7 +26,7 @@ const Community = () => {
       <section className="w-[33.3%]">
         <div className="content h-[30rem] relative" style={{ backgroundImage: `url(${cardimg2.src})`, backgroundSize: 'cover', backgroundPosition: 'center' }}>
           <div className="p-4 absolute bottom-0 left-0 right-0 text-center pb-[5rem]">
-            <h2 className="text-white text-center font-semibold">Prophetic session</h2>
+            <h2 className="text-white text-center font-semibold text-lg md:text-2xl xl:text-2xl 2xl:text-2xl">Prophetic session</h2>
             <p className="text-white text-center font-semibold text-lg md:text-xl">REQUEST<br />A<br />PRAYER SESSION</p>
             <button className="bg-red-600 text-white  md:w-[50%] lg:w-[50%] xl:w-[50%] 2xl:w-[50%]  p-3 mt-5 ">LETS PRAY</button>
           </div>
--- src/app/components/common/Mainslidder.tsx
+++ src/app/components/common/Mainslidder.tsx
@@ -94,7 +94,7 @@ const FreeStyle = () => {
               className="w-full  object-cover mb-4 xl:h-[40rem] 2xl:h-[40rem] lg:h-[30rem] md:h-[33rem] h-[17rem]"
             />
             <div id='imgtext' className='absolute font-bold top-0 left-0 w-full h-full text-white text-[2.9rem] m-[5rem] 2xl:text-[5rem] 2xl:m-[10rem] md:mt-[4rem] xl:mt-[5rem] lg:mt-[4rem] 2xl:mt-[10rem] mt-[3.4rem]'>
-              <h2>The Word</h2>
+              <h2>Word</h2>
               <h2>Tabernacle</h2>
               <p className='text-[1.1rem] font-normal'>Building word practitioners...</p>
               <div>
