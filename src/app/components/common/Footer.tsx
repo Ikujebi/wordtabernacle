c43c588891c3edd5ee828949e31e06d531787381
Ikujebi <133238121+Ikujebi@users.noreply.github.com>
2024-07-22 14:22:21 +0100

    Update Footer.tsx

--- src/app/components/common/Footer.tsx
+++ src/app/components/common/Footer.tsx
@@ -6,8 +6,17 @@ import { FaFacebookF, FaInstagram, FaTwitter, FaYoutube, FaTiktok, FaTelegram } 
 
 
 const Footer = () => {
+  const socialLinks = [
+    { href: "https://x.com/wordtabernaclen", icon: FaTwitter,id:1 },
+    { href: "https://www.instagram.com/wordtabernacleng/", icon: FaInstagram,id:2 },
+    { href: "https://www.facebook.com/wordtabernacle", icon: FaFacebookF,id:3 },
+    { href: "https://www.youtube.com/@wordtabernacleng", icon: FaYoutube,id:4 },
+    { href: "https://www.tiktok.com/@wordtabernacleng", icon: FaTiktok,id:5 },
+    { href: "https://t.me/+ufId9mRBMlVlMzY8", icon: FaTelegram,id:5 },
+   
+  ];
   return (
-    <div className="bg-black text-white pt-16 font">
+    <div className="bg-black text-white pt-16 font w-[100svw]">
       <div className="container mx-auto">
         <div className="text-center pb-4">
           <h3 className="text-xl">Subscribe for updates</h3>
@@ -27,26 +36,13 @@ const Footer = () => {
             <p className="mt-2 text-sm">
               By subscribing to Word tabernacle Newsletter, you consent to receive frequent information regarding the church via automated technology
             </p>
-            <div className="flex justify-center mt-4">
-            <Link href={"https://x.com/wordtabernaclen"} className="md:w-8 lg:w-8 xl:w-8 2xl:w-8 w-6 md:h-8 lg:h-8 xl:h-8 2xl:h-8 h-6 bg-red-600 rounded-full flex items-center justify-center">
-                            <FaTwitter className="text-white text-lg" />
-                        </Link>
-                        <Link href={"https://www.instagram.com/wordtabernacleng/"} className="md:w-8 lg:w-8 xl:w-8 2xl:w-8 w-6 md:h-8 lg:h-8 xl:h-8 2xl:h-8 h-6 bg-red-600 rounded-full flex items-center justify-center">
-                            <FaInstagram className="text-white text-lg" />
-                        </Link>
-                        <Link href={"https://www.facebook.com/wordtabernacle"} className="md:w-8 lg:w-8 xl:w-8 2xl:w-8 w-6 md:h-8 lg:h-8 xl:h-8 2xl:h-8 h-6 bg-red-600 rounded-full flex items-center justify-center">
-                            <FaFacebookF className="text-white text-lg" />
-                        </Link>
-                        <Link href={"https://www.youtube.com/@wordtabernacleng"} className="md:w-8 lg:w-8 xl:w-8 2xl:w-8 w-6 md:h-8 lg:h-8 xl:h-8 2xl:h-8 h-6 bg-red-600 rounded-full flex items-center justify-center">
-                            <FaYoutube className="text-white text-lg" />
-                        </Link>
-                        <Link href={"https://www.tiktok.com/@wordtabernacleng"} className="md:w-8 lg:w-8 xl:w-8 2xl:w-8 w-6 md:h-8 lg:h-8 xl:h-8 2xl:h-8 h-6 bg-red-600 rounded-full flex items-center justify-center">
-                            <FaTiktok className="text-white text-lg" />
-                        </Link>
-                        <Link href={"https://t.me/+ufId9mRBMlVlMzY8"} className="md:w-8 lg:w-8 xl:w-8 2xl:w-8 w-6 md:h-8 lg:h-8 xl:h-8 2xl:h-8 h-6 bg-red-600 rounded-full flex items-center justify-center">
-                            <FaTelegram className="text-white text-lg" />
-                        </Link>
-                        <a href={"mailto:wordtabernaclebc@gmail.com"} className="ml-2  items-center">
+            <div className="flex justify-center mt-4 gap-2">
+            {socialLinks.map(({ href, icon: Icon }, id) => (
+                <Link key={id} href={href} className="md:w-8 lg:w-8 xl:w-8 2xl:w-8 w-6 md:h-8 lg:h-8 xl:h-8 2xl:h-8 h-6 bg-red-600 rounded-full flex items-center justify-center">
+                  <Icon className="text-white text-lg" />
+                </Link>
+              ))}
+                        <a href={"mailto:wordtabernaclebc@gmail.com"} className="md:w-8 lg:w-8 xl:w-8 2xl:w-8 w-6 md:h-8 lg:h-8 xl:h-8 2xl:h-8 h-6 bg-red-600 rounded-full flex items-center justify-center">
                             <MdEmail />
                             
                         </a>
