7058b57019140f2eb793f4ddfa2da098e6ed86a3
Ikujebi <133238121+Ikujebi@users.noreply.github.com>
2024-08-31 17:44:29 +0100

    Update page.tsx

--- src/app/online/page.tsx
+++ src/app/online/page.tsx
@@ -3,8 +3,8 @@ import { FC, useEffect, useState } from "react";
 import Header from "../components/common/Header";
 import online from "../img/online.webp";
 import useSetField from "../../custom-hooks/useSetField";
-import NextImage from "next/image"
-import Link from "next/link"
+import NextImage from "next/image";
+import Link from "next/link";
 
 const Page: FC = () => {
   const [state, setState] = useState<{ programmeId: number }>({ programmeId: 1 });
@@ -13,17 +13,17 @@ const Page: FC = () => {
     {
       img: online.src,
       title: "WATCH ON YOUTUBE",
-      link: "online/youtube"
+      link: "online/youtube",
     },
     {
       img: online.src,
       title: "LISTEN ON MXLR",
-      link: "online/mxlr"
+      link: "online/mxlr",
     },
     {
       img: online.src,
       title: "LISTEN ON WORDLISTEN",
-      link: "online/mxlr"
+      link: "online/mxlr",
     },
   ];
 
@@ -31,7 +31,7 @@ const Page: FC = () => {
 
   const getProgrammeName = (programmeId: number) => {
     if (programmeId === 1) {
-      return "TWATCH ON YOUTUBE";
+      return "WATCH ON YOUTUBE";
     } else if (programmeId === 2) {
       return "LISTEN ON MXLR";
     } else {
@@ -46,38 +46,34 @@ const Page: FC = () => {
     }));
   }, [state.programmeId]);
 
-  const videoId = "U2c6EMa9NJY?si";
-
   return (
     <div>
       <div>
-      <Header />
+        <Header />
       </div>
 
-      <div className="xl:w-[60%] md:w-[60%] mx-auto grid gap-5 h-[100svh] pt-[7.5rem] md:pt-[9rem] lg:pt-[10rem] xl:pt-[10rem] 2xl:pt-[10rem]">
+      <div className="xl:w-[60%] md:w-[60%] mx-auto grid h-[100svh] pt-[7.5rem] md:pt-[9rem] lg:pt-[9rem] xl:pt-[9rem] 2xl:pt-[10rem]">
         {data.map((item, index) => (
           <Link
-          href={item.link}
+            href={item.link}
             key={index}
             onClick={() => setRequest("programmeId", index + 1)}
-            style={{ background: state.programmeId === index + 1 ? "#41c0b7" : "inherit" }}
-            className={`flex h-[90%] cursor-pointer ${state.programmeId === index + 1 ? 'bg-[#41c0b7]' : 'bg-inherit'}`}
-          
+            style={{ background: "#41c0b7" }}
+            className={`flex items-center md:h-[11rem] lg:h-[11rem] xl:h-[11rem] h-[90%] bg-[#41c0b7] cursor-pointer ${state.programmeId === index + 1 ? 'bg-[#41c0b7]' : 'bg-inherit'} m-0 p-0`}
           >
-             <NextImage
+            <NextImage
               src={item.img}
               alt={item.title}
               width={144} // Ensure the correct width is passed
-              height={80} // Ensure the correct height is passed
-              className="object-cover w-[40%] my-[1rem] rounded-2xl ml-4"
+              height={50} // Ensure the correct height is passed
+              className="w-[40%] h-[95%] my-0 ml-4 rounded-2xl "
             />
-            <p id="text" className="font-bold md:text-xl lg:text-xl xl:text-2xl 2xl:text-2xl  flex justify-left items-center mx-auto">{item.title}</p>
+            <p id="text" className="font-bold md:text-xl lg:text-xl xl:text-2xl 2xl:text-2xl flex justify-left items-center mx-auto">
+              {item.title}
+            </p>
           </Link>
         ))}
       </div>
-
-     
-     
     </div>
   );
 };
