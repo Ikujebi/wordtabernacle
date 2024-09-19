7b2d26d511b07ad235995846142afbb0172b5809
Ikujebi <133238121+Ikujebi@users.noreply.github.com>
2024-09-19 12:04:05 +0100

    Update page.tsx

--- src/app/online/page.tsx
+++ src/app/online/page.tsx
@@ -9,14 +9,16 @@ import Link from "next/link";
 const Page: FC = () => {
   const [state, setState] = useState<{ programmeId: number }>({ programmeId: 1 });
 
+  const youtubeImage = "https://res.cloudinary.com/dalylashp/image/upload/v1726739640/word_youtube_xqdcku.png";
+const mixlr = "https://res.cloudinary.com/dalylashp/image/upload/v1726743756/wordmix_ssi1z9.webp"
   const data = [
     {
-      img: online.src,
+      img: youtubeImage,
       title: "WATCH ON YOUTUBE",
       link: "online/youtube",
     },
     {
-      img: online.src,
+      img: mixlr,
       title: "LISTEN ON MXLR",
       link: "online/mxlr",
     },
@@ -30,12 +32,13 @@ const Page: FC = () => {
   const { setRequest } = useSetField(setState);
 
   const getProgrammeName = (programmeId: number) => {
-    if (programmeId === 1) {
-      return "WATCH ON YOUTUBE";
-    } else if (programmeId === 2) {
-      return "LISTEN ON MXLR";
-    } else {
-      return "LISTEN ON WORDLISTEN";
+    switch (programmeId) {
+      case 1:
+        return "WATCH ON YOUTUBE";
+      case 2:
+        return "LISTEN ON MXLR";
+      default:
+        return "LISTEN ON WORDLISTEN";
     }
   };
 
@@ -47,31 +50,31 @@ const Page: FC = () => {
   }, [state.programmeId]);
 
   return (
-    <div>
-      <div>
-        <Header />
-      </div>
-
-      <div className="xl:w-[60%] md:w-[60%] mx-auto grid h-[100svh] pt-[7.5rem] md:pt-[9rem] lg:pt-[9rem] xl:pt-[9rem] 2xl:pt-[10rem]">
+    <div className="h-[100svh] overflow-hidden">
+      <Header />
+      <div className="flex flex-wrap justify-center items-center  h-[100svh] pt-[7.5rem] md:pt-[9rem] lg:pt-[9rem] xl:pt-[9rem] 2xl:pt-[10rem] overflow-hidden">
         {data.map((item, index) => (
-          <Link
-            href={item.link}
+          <div
             key={index}
-            onClick={() => setRequest("programmeId", index + 1)}
-            style={{ background: "#41c0b7" }}
-            className={`flex items-center md:h-[11rem] lg:h-[11rem] xl:h-[11rem] h-[90%] bg-[#41c0b7] cursor-pointer ${state.programmeId === index + 1 ? 'bg-[#41c0b7]' : 'bg-inherit'} m-0 p-0`}
+            className="inline-block w-[calc(45%-1rem)] sm:w-[calc(45%%-1rem)] lg:w-[calc(45%-1rem)] xl:w-[calc(45%-1rem)] px-2 mb-4 text-center"
           >
-            <NextImage
-              src={item.img}
-              alt={item.title}
-              width={144} // Ensure the correct width is passed
-              height={50} // Ensure the correct height is passed
-              className="w-[40%] h-[95%] my-0 ml-4 rounded-2xl "
-            />
-            <p id="text" className="font-bold md:text-xl lg:text-xl xl:text-2xl 2xl:text-2xl flex justify-left items-center mx-auto">
-              {item.title}
-            </p>
-          </Link>
+            <Link
+              href={item.link}
+              onClick={() => setRequest("programmeId", index + 1)}
+              className={` block h-[14rem] p-2 w-full shadow-xl cursor-pointer overflow-hidden rounded-md ${state.programmeId === index + 1 ? 'shadow-xl' : ''}`}
+            >
+              <NextImage
+                src={item.img}
+                alt={item.title}
+                width={144}
+                height={50}
+                className="w-full h-[90%] my-0 p-1 rounded-2xl"
+              />
+              <b className="font-bold md:text-xl lg:text-xl xl:text-2xl 2xl:text-2xl">
+                {item.title}
+              </b>
+            </Link>
+          </div>
         ))}
       </div>
     </div>
