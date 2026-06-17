84713518c45306106fff099e9d095b8dd4607a33
Ikujebi <133238121+Ikujebi@users.noreply.github.com>
2026-06-17 09:39:48 +0100

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,16 @@
 import type { Metadata } from "next";
 import { Montserrat } from "@/app/fonts";
 import "./globals.css";
-import logo from "../app/img/wordlogo.png";
-import Head from "next/head";
-import Header from "./components/common/Header"
-import Footer from "./components/common/Footer"
-
-
-
+import Header from "./components/common/Header";
+import Footer from "./components/common/Footer";
 
 export const metadata: Metadata = {
   title: "Word Tabernacle Bible church",
-  description: "Word Tabernacle official website",
+  description: "Word Tabernacle Bible church official website",
+  icons: {
+    icon: "/favicon.ico",
+    shortcut: "/favicon.ico",
+  },
 };
 
 export default function RootLayout({
@@ -21,19 +20,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <Head>
-        <link rel="icon" href={`${logo}`} sizes="any" />
-        <link rel="shortcut icon" href={`${logo}`} type="image/x-icon" />
-      </Head>
       <body className={Montserrat.className}>
-          
-        {/* Global Header */}
         <Header />
 
-        {/* Page Content */}
         <main>{children}</main>
 
-        {/* Global Footer */}
         <Footer />
       </body>
     </html>
